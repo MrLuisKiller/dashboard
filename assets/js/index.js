d6c106655b9bd970219ee75a0fa4b2e4f6f0b76b
MrLuisKiller <luis_bol_1998@hotmail.com>
2023-11-09 22:23:44 -0700

    Chart Js graph whit the value of the currency of Mexico, Canada and the Euro (default)

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,15 @@
 import { API, PreSave, Codes } from "./var.js"
+import { BackgroundColor, BorderColor } from "./chart.js"
 
 const ddCodes = document.getElementById('ddCodes')
 const tBody = document.getElementById('tBody')
+const ctx = document.getElementById('chart')
 
 let preSave = JSON.parse(localStorage.getItem('preSave')) || PreSave
+let chart = null
+let codes = {}
+let labels = []
+let rates = []
 
 const headers = {
     "Accept": "*/*",
@@ -12,6 +18,9 @@ const headers = {
 
 window.addEventListener('load', async () => {
     const date = `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`
+
+    codes = Codes.concat(preSave.persCodes)
+    codes.splice(0, 1)
 
     if (Object.keys(preSave.codeList).length == 0) {
         preSave.codeList = await fetch(`https://v6.exchangerate-api.com/v6/${API}/codes`, { headers: headers })
@@ -27,4 +36,28 @@ window.addEventListener('load', async () => {
         preSave.historical.last_update = date
         localStorage.setItem('preSave', JSON.stringify(preSave))
     }
+
+    codes.forEach(code => {
+        labels.push(code[1])
+        rates.push(preSave.historical.rates[code[0]])
+    })
+
+    chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Price per Dollar',
+                data: rates,
+                backgroundColor: BackgroundColor,
+                borderColor: BorderColor,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: { beginAtZero: true }
+            }
+        }
+    })
 })
