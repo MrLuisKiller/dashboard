1716fe8a506350116f9dd1de3c0d5b586c3689bb
MrLuisKiller <luis_bol_1998@hotmail.com>
2023-11-10 00:16:44 -0700

    Currency loading in the graph and table, elimination of said currency, a maximum of 7. Default loading if it does not detect saved

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,23 +4,27 @@ import { BackgroundColor, BorderColor } from "./chart.js"
 const ddCodes = document.getElementById('ddCodes')
 const tBody = document.getElementById('tBody')
 const ctx = document.getElementById('chart')
-
-let preSave = JSON.parse(localStorage.getItem('preSave')) || PreSave
-let chart = null
-let codes = {}
-let labels = []
-let rates = []
-
 const headers = {
     "Accept": "*/*",
     "Content-Type": "application/json; charset=utf-8"
 }
 
+let preSave = JSON.parse(localStorage.getItem('preSave')) || PreSave
+let chart = null
+let labels = []
+let rates = []
+
+
 window.addEventListener('load', async () => {
     const date = `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`
+    let codes
 
-    codes = Codes.concat(preSave.persCodes)
-    codes.splice(0, 1)
+    if (preSave.persCodes.length > 0)
+        codes = preSave.persCodes
+    else {
+        codes = Codes
+        codes.splice(0, 1)
+    }
 
     if (Object.keys(preSave.codeList).length == 0) {
         preSave.codeList = await fetch(`https://v6.exchangerate-api.com/v6/${API}/codes`, { headers: headers })
@@ -65,15 +69,35 @@ window.addEventListener('load', async () => {
     loadTable()
 })
 
+ddCodes.addEventListener('change', () => {
+    const code = preSave.codeList.find(code => code[0] == ddCodes.value)
+
+    if (preSave.persCodes.length < 4) {
+        preSave.persCodes.push(code)
+        localStorage.setItem('preSave', JSON.stringify(preSave))
+    }
+
+    labels.push(code[1])
+    rates.push(preSave.historical.rates[code[0]])
+    chart.update()
+    loadDropDownList()
+    loadTable()
+
+})
+
 const loadDropDownList = () => {
+    validateList()
+
     ddCodes.innerHTML = '';
     ddCodes.innerHTML = '<option selected>Choose...</option>'
     preSave.codeList.map(el => {
         let exist = false
+
         labels.forEach(label => {
             if (el[1] == label)
                 exist = true
         })
+
         if (!exist)
             ddCodes.innerHTML += `<option id="${el[0]}" value="${el[0]}">${el[1]}</option>`
     })
@@ -81,10 +105,12 @@ const loadDropDownList = () => {
 
 const loadTable = () => {
     tBody.innerHTML = ''
+    
     if (labels.length > 0)
         labels.forEach(label => {
             let index = labels.findIndex(labelI => labelI == label)
             let code = ''
+
             preSave.codeList.forEach(codeL => {
                 if (codeL[1] == label)
                     code = codeL[0]
@@ -104,6 +130,7 @@ const loadTable = () => {
             let i = document.createElement('i')
             i.classList.add('bi')
             i.classList.add('bi-trash3')
+            i.onclick = () => deleteCode(label)
             i.classList.add('text-danger')
             tdButton.appendChild(i)
             tr.appendChild(tdCode)
@@ -112,4 +139,32 @@ const loadTable = () => {
             tr.appendChild(tdButton)
             tBody.appendChild(tr)
         })
+    else
+        tBody.innerHTML = `<tr>
+            <td class="table-danger text-center">No codes Selected</td>
+        </tr>`
+}
+
+const validateList = () => ddCodes.disabled = !(preSave.persCodes.length < 4 || labels.length < 7)
+
+const deleteCode = label => {
+    let index
+
+    preSave.persCodes.forEach(code => {
+        if (code[1] == label) {
+            index = preSave.persCodes.findIndex(code => code[1] == label)
+            preSave.persCodes.splice(index, 1)
+            localStorage.setItem('preSave', JSON.stringify(preSave))
+        }
+    })
+    index = labels.findIndex(labelI => labelI == label)
+    deleteData(index)
+    loadDropDownList()
+    loadTable()
+}
+
+const deleteData = index => {
+    labels.splice(index, 1)
+    rates.splice(index, 1)
+    chart.update()
 }
